c267361b7715910d1b41f2c4d56b0a48b6c59b05
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-01 13:01:42 +0800

    Simplified includes.

--- imgui/imgui_win32_customimpl.cpp
+++ imgui/imgui_win32_customimpl.cpp
@@ -2,10 +2,10 @@
 #include <d3d9.h>
 #include "../imgui/imgui_impl_dx9.h"
 #include "../imgui/imgui_impl_win32.h"
-#include <Windows.h>
 #include <vector>
 
-namespace DXHook {
+namespace DXHook
+{
 	// Updates the input things, why not use the normal backend? too cursed for me to go through--also this is
 	// mainly for True Fullscreen, since ImGui will break on that because of cursed reasons I don't even know about..
 	std::vector<int> keyCodes = {
@@ -25,33 +25,42 @@ namespace DXHook {
 		VK_DELETE // more are in here, just in mainDX.cpp
 	};
 
-	void UpdateImGUI() {
+	void UpdateImGUI()
+	{
 		ImGuiIO& io = ImGui::GetIO();
 		POINT currentMousePos;
 		GetCursorPos(&currentMousePos);
 
 		io.MousePos = ImVec2(currentMousePos.x, currentMousePos.y);
 
-		if ((GetKeyState(VK_LBUTTON) & 0x8000) != 0) { // mouse down
+		if ((GetKeyState(VK_LBUTTON) & 0x8000) != 0)
+		{ // mouse down
 			io.MouseDown[0] = true;
 		}
-		else {
+		else
+		{
 			io.MouseDown[0] = false;
 		}
 
-		if ((GetKeyState(VK_RBUTTON) & 0x8000) != 0) { // mouse right down
+		if ((GetKeyState(VK_RBUTTON) & 0x8000) != 0)
+		{ // mouse right down
 			io.MouseDown[1] = true;
 		}
-		else {
+		else
+		{
 			io.MouseDown[1] = false;
 		}
 
-		if (io.WantCaptureMouse) { // do not ask
-			for (int keyCode : keyCodes) {
-				if ((GetKeyState(keyCode) & 0x8000) != 0) {
+		if (io.WantCaptureMouse)
+		{ // do not ask
+			for (int keyCode : keyCodes)
+			{
+				if ((GetKeyState(keyCode) & 0x8000) != 0)
+				{
 					io.KeysDown[keyCode] = true;
 				}
-				else {
+				else
+				{
 					io.KeysDown[keyCode] = false;
 				}
 			}
