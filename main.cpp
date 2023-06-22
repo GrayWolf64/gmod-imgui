c7af703d62bb2ab746963ed3f4189168b2a297eb
GrayWolf <tairikuookami@foxmail.com>
2023-06-22 15:09:54 +0800

    Updated project.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #define GMMODULE
 
 #include "Interface.h"
+#include "kiero.h"
+#include "d3d9.h"
+#include "Windows.h"
+#include "minhook/include/MinHook.h"
+#include "backends/imgui_impl_win32.h"
+#include "backends/imgui_impl_dx9.h"
 
 // Called when the module opens
 GMOD_MODULE_OPEN()
 {
-    return 0;
+	long long pid = GetCurrentProcessId();
+    LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
+    LUA->GetField(-1, "print");
+    LUA->PushNumber(pid);
+    LUA->Call(1, 0);
+    LUA->Pop();
+	return 0;
 }
+
 // Called when the module closes
 GMOD_MODULE_CLOSE()
 {
