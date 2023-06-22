e26abfc01006d59edd4b8544bf01b0db2c7b9618
GrayWolf <tairikuookami@foxmail.com>
2023-06-22 19:57:13 +0800

    Init demo test succeeded.

--- main.cpp
+++ main.cpp
@@ -8,20 +8,111 @@
 #include "backends/imgui_impl_win32.h"
 #include "backends/imgui_impl_dx9.h"
 
-// Called when the module opens
 GMOD_MODULE_OPEN()
 {
-	long long pid = GetCurrentProcessId();
-    LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
-    LUA->GetField(-1, "print");
-    LUA->PushNumber(pid);
-    LUA->Call(1, 0);
-    LUA->Pop();
 	return 0;
 }
 
-// Called when the module closes
 GMOD_MODULE_CLOSE()
 {
     return 0;
+}
+
+static WNDPROC oWndProc = NULL;
+static HWND gmWindow = NULL;
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+typedef long(__stdcall* Reset)(LPDIRECT3DDEVICE9, D3DPRESENT_PARAMETERS*);
+static Reset oReset = NULL;
+
+typedef long(__stdcall* EndScene)(LPDIRECT3DDEVICE9);
+static EndScene oEndScene = NULL;
+
+long __stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice)
+{
+	static bool init = false;
+
+	if (!init)
+	{
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO();
+		io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
+		ImGui_ImplWin32_Init(gmWindow);
+		ImGui_ImplDX9_Init(pDevice);
+
+		init = true;
+	}
+
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::ShowDemoWindow();
+
+	ImGui::EndFrame();
+	ImGui::Render();
+	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+
+	return oEndScene(pDevice);
+}
+
+LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+	if (true && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+		return true;
+
+	return CallWindowProc(oWndProc, hWnd, uMsg, wParam, lParam);
+}
+
+
+BOOL CALLBACK EnumWindowsCallback(HWND handle, LPARAM lParam)
+{
+	DWORD wndProcId;
+	GetWindowThreadProcessId(handle, &wndProcId);
+
+	if (GetCurrentProcessId() != wndProcId)
+		return TRUE;
+
+	gmWindow = handle;
+	return FALSE;
+}
+
+HWND GetProcessWindow()
+{
+	gmWindow = NULL;
+	EnumWindows(EnumWindowsCallback, NULL);
+	return gmWindow;
+}
+
+DWORD WINAPI MainThread(LPVOID lpReserved)
+{
+	bool attached = false;
+	do
+	{
+		if (kiero::init(kiero::RenderType::D3D9) == kiero::Status::Success)
+		{
+			kiero::bind(42, (void**)&oEndScene, hkEndScene);
+			do
+				gmWindow = GetProcessWindow();
+			while (gmWindow == NULL);
+			oWndProc = (WNDPROC)SetWindowLongPtr(gmWindow, GWL_WNDPROC, (LONG_PTR)WndProc);
+			attached = true;
+		}
+	} while (!attached);
+	return TRUE;
+}
+
+BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hMod);
+		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		kiero::shutdown();
+		break;
+	}
+	return TRUE;
 }
